7e7fa2bac3b64fcb63899271792bc5874c92574d
mg143pavankumar <pavansharma.mg143@gmail.com>
2024-01-04 15:33:23 -0500

    issue: fixed eslint issue

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,13 +6,16 @@ export interface ButtonProps
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
+    const buttonClasses = cn(
+      "w-auto rounded-full bg-black border-transparent px-5 py-3 text-white font-semibold hover:opacity-75 transition disabled:cursor-not-allowed disabled:opacity-50",
+      className
+    );
+
     return (
       <button
+        type={type}
         disabled={disabled}
-        className={cn(
-          `w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`,
-          className
-        )}
+        className={buttonClasses}
         ref={ref}
         {...props}
       >
@@ -21,5 +24,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     );
   }
 );
+
+Button.displayName = "Button"; // It's a good practice to set displayName when using forwardRef
 
 export default Button;
