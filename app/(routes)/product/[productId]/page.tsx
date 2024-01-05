b4bcfbbd00e785dc32a8b56a741fb349a27e232e
mg143pavankumar <pavansharma.mg143@gmail.com>
2024-01-04 19:47:24 -0500

    fixed: types issues in products

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -24,11 +24,11 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 ">
             {/* Gallery */}
-            <Gallery images={productData?.images} />
+            <Gallery images={productData?.images || []} />
 
             {/* Info */}
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              <Info data={productData} />
+              {productData && <Info data={productData} />}
             </div>
           </div>
         </div>
